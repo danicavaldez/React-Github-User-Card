dc07ce7df19a8a95ab1787dffc59b3fac0432bef
Danica Valdez <danicavaldez@Danicas-MacBook-Pro.local>
2019-09-09 19:56:13 -0700

    displaying user and follower data

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import axios from 'axios';
+import React, { Component } from 'react';
+import axios from "axios";
 
-class App extends React.Component {
 
-  constructor() {
-    super();
-    this.state = {
-      user: []
-    }
+class App extends Component {
+  state = {
+    user: {},
+    followers: []
   }
 
   componentDidMount() {
@@ -15,17 +13,40 @@ class App extends React.Component {
       .get('https://api.github.com/users/danicavaldez')
       .then(res => {
         console.log(res.data)
-        this.setState(({userInfo: res}))
+        this.setState({user: res.data})
       })
-      .catch(err => console.error(err))
-  } 
+      .catch(err => console.error("BROKEN:", err))
+
+    axios
+    .get('https://api.github.com/users/danicavaldez/followers')  
+    .then(res => {
+      console.log(res.data)
+      this.setState({followers: res.data})
+    })
+    .catch(err => console.error("BROKEN:", err))
+  }
 
   render() {
     return(
-      <div></div>
+      <>
+        <div>
+          <img src={this.state.user.avatar_url} /> 
+          <h2>{this.state.user.name}</h2>
+          <p>{this.state.user.login}</p>
+          <p>Followers: {this.state.user.followers}</p>
+          <p>Following: {this.state.user.following}</p>
+        </div>
+
+        <div>
+          <h1>Followers</h1>
+          {this.state.followers.map(follower => (
+            <><img width="100" src={follower.avatar_url} key={follower.id} alt={follower} />
+            <h3>{follower.login}</h3></>
+          ))}
+        </div>
+      </>
     )
   }
-
 }
 
 export default App;
